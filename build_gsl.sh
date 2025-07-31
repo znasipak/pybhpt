47dd4b083f1f248ce4de7b245aa318e87fd930d4
Zach Nasipak <13366660+znasipak@users.noreply.github.com>
2025-07-31 19:39:41 +0100

    Attempt to build gsl from source

--- build_gsl.sh
+++ build_gsl.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
-set -euo pipefail
-
-GSL_VERSION=2.7.1
-INSTALL_DIR="$(pwd)/gsl-install"
-
-echo "Downloading GSL ${GSL_VERSION}..."
-curl -fsSL -O https://ftp.gnu.org/gnu/gsl/gsl-${GSL_VERSION}.tar.gz
-tar -xzf gsl-${GSL_VERSION}.tar.gz
-cd gsl-${GSL_VERSION}
-
-echo "Configuring GSL..."
-./configure --prefix="${INSTALL_DIR}" --disable-shared --enable-static
-make -j$(nproc || sysctl -n hw.ncpu)
-make install
-
-# Output CMAKE_PREFIX_PATH for later use
-echo "GSL installed to ${INSTALL_DIR}"
-echo "CMAKE_PREFIX_PATH=${INSTALL_DIR}" >> $GITHUB_ENV
+wget https://ftp.gnu.org/gnu/gsl/gsl-2.7.1.tar.gz
+tar -xzf gsl-2.7.1.tar.gz
+cd gsl-2.7.1
+./configure --prefix=/usr/local
+make -j$(nproc)
+sudo make install
 
