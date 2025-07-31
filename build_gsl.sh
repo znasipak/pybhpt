b5c348a9aab22a999922f29c896a1fe1d65ad4aa
Zach Nasipak <13366660+znasipak@users.noreply.github.com>
2025-07-31 19:58:09 +0100

    New compile script for GSL

--- build_gsl.sh
+++ build_gsl.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-wget https://ftp.gnu.org/gnu/gsl/gsl-2.7.1.tar.gz
-tar -xzf gsl-2.7.1.tar.gz
-cd gsl-2.7.1
-./configure --prefix=/usr/local
+GSL_VERSION=2.8
+INSTALL_DIR="/opt/gsl-${GSL_VERSION}"
+
+mkdir -p /tmp/gsl-src
+cd /tmp/gsl-src
+curl -LO https://ftp.gnu.org/gnu/gsl/gsl-${GSL_VERSION}.tar.gz
+tar -xzf gsl-${GSL_VERSION}.tar.gz
+cd gsl-${GSL_VERSION}
+./configure --prefix="${INSTALL_DIR}"
 make -j$(nproc)
-sudo make install
+make install
+
+echo "CMAKE_PREFIX_PATH=${INSTALL_DIR}" >> $GITHUB_ENV
+
 
